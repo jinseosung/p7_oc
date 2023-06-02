71d9ebdee64eda34669424217bcd2d0e98b4e463
Jinseo Sung <jinseosung15@gmail.com>
2023-06-02 16:54:40 +0200

    connecting to MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const uri =
+  "mongodb+srv://p7_oc:p7_OC@monvieuxgrimoire.bhqzffn.mongodb.net/?retryWrites=true&w=majority";
 
 const booksRoutes = require("./routes/book");
 const userRoutes = require("./routes/user");
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 app.use(express.json());
