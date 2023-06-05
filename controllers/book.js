6d636674d09c60a044e3d7cf59d3be2b5f3cd0b1
Jinseo Sung <jinseosung15@gmail.com>
2023-06-05 20:18:34 +0200

    configure updateBook Controller

--- controllers/book.js
+++ controllers/book.js
@@ -30,7 +30,34 @@ exports.getBook = (req, res, next) => {
 };
 
 exports.updateBook = (req, res, next) => {
-  res.status(200).json({ message: "updatebook" });
+  const bookObject = req.file
+  ? {
+      ...JSON.parse(req.body.book),
+      imageUrl: `${req.protocol}://${req.get("host")}/images/${
+        req.file.filename
+      }`,
+    }
+  : { ...req.body };
+
+delete bookObject._userId;
+
+Book.findOne({ _id: req.params.id })
+  .then((book) => {
+    if (book.userId != req.auth.userId) {
+      res.status(401).json({ message: "Non autorisé" });
+      return;
+    }
+    Book.updateOne(
+      { _id: req.params.id },
+      { ...bookObject, _id: req.params.id }
+    )
+      .then(() => {
+        res.status(200).json({ message: "Livre modifié" });
+      })
+      .catch((error) => res.status(401).json({ error }));
+  })
+  .catch((error) => res.status(400).json({ error }));
+
 };
 
 exports.getRating = (req, res, next) => {
