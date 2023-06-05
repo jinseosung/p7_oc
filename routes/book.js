3090bddb43fdb7d9d6f65af766ed5ca5141aa27a
Jinseo Sung <jinseosung15@gmail.com>
2023-06-05 15:33:15 +0200

    create user authentification middleware

--- routes/book.js
+++ routes/book.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const bookCtrl = require("../controllers/book");
+const auth = require("../middleware/auth");
 
 const router = express.Router();
 
 router.get("/", bookCtrl.getAllBooks);
 router.get("/:id", bookCtrl.getBook);
 router.get("/bestrating", bookCtrl.getRating);
-router.post("/", bookCtrl.createBook);
-router.put("/:id", bookCtrl.updateBook);
-router.delete("/:id", bookCtrl.deleteBook);
-router.post("/:id/rating", bookCtrl.updateRating);
+router.post("/", auth, bookCtrl.createBook);
+router.put("/:id", auth, bookCtrl.updateBook);
+router.delete("/:id", auth, bookCtrl.deleteBook);
+router.post("/:id/rating", auth, bookCtrl.updateRating);
 
 module.exports = router;
