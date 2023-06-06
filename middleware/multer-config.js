1c4ba03964946b20481651f5bdbb7dd1ed5ccdb2
Jinseo Sung <jinseosung15@gmail.com>
2023-06-06 18:41:11 +0200

    add file type validation to multer-config.js

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,19 +1,18 @@
 const multer = require("multer");
-
-const MIME_TYPES = {
-  "image/jpg": "jpg",
-  "image/jpeg": "jpg",
-  "image/png": "png",
-};
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, "images");
   },
   filename: (req, file, callback) => {
+    if (file.mimetype.split("/")[0] !== "image") {
+      callback(new Error(`Ce fichier n'est pas une image`));
+      return;
+    }
     const name = file.originalname.split(" ").join("_");
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + "." + extension);
+    const ext = path.extname(file.originalname);
+    callback(null, name + Date.now() + "." + ext);
   },
 });
 
