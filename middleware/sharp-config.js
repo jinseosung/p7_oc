555532cda161238030c02598522e1374a3029495
Jinseo Sung <jinseosung15@gmail.com>
2023-07-03 23:39:31 +0200

    add if to generate req.file to sharp

--- middleware/sharp-config.js
+++ middleware/sharp-config.js
@@ -2,12 +2,14 @@ const sharp = require("sharp");
 const path = require("path");
 
 const sharpConfig = (req, res, next) => {
-  const buffer = req.file.buffer;
-  const originalFileName = path.parse(req.file.originalname).name;
-  const newFileName = `${originalFileName}${Date.now()}.webp`;
-  req.file.filename = newFileName;
+  if (req.file) {
+    const buffer = req.file.buffer;
+    const originalFileName = path.parse(req.file.originalname).name;
+    const newFileName = `${originalFileName}${Date.now()}.webp`;
+    req.file.filename = newFileName;
 
-  sharp(buffer).resize({ width: 360 }).toFile(`images/${newFileName}`);
+    sharp(buffer).resize({ width: 360 }).toFile(`images/${newFileName}`);
+  }
   next();
 };
 
